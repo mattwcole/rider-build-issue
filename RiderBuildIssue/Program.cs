6d86b8965a787e3484fc2e1530d6c03379ec2d76
Matt Cole <matt-cole@hotmail.co.uk>
2017-03-28 00:04:56 +0100

    Initial commit

---
+++ RiderBuildIssue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RiderBuildIssue
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var ipAddress = GetIpAddressAsync().Result;
+            Console.WriteLine($"Your IP address is {ipAddress.Ip}");
+        }
+
+        public static async Task<IpAddress> GetIpAddressAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("http://ip.jsontest.com");
+                response.EnsureSuccessStatusCode();
+
+                return await response.Content.ReadAsAsync<IpAddress>();
+            }
+        }
+    }
+
+    public class IpAddress
+    {
+        public string Ip { get; set; }
+    }
+}
